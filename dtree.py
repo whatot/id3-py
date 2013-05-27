8654c1620b4cbf6f048758d1155b7f726ff1f4f6
whatot <whatot2@gmail.com>
2013-05-27 14:40:12 +0800

    redesign the code(pep8) and add python3 support

--- dtree.py
+++ dtree.py
@@ -5,19 +5,20 @@ ID3算法,创建决策数
 
 import math
 
+
 def entropy(data, target_attr):
     """
     Calculates the entropy of the given data set for the target attribute.
     """
-    val_freq     = {}
+    val_freq = {}
     data_entropy = 0.0
 
     # Calculate the frequency of each of the values in the target attr
     for record in data:
-        if (val_freq.has_key(record[target_attr])):
+        if (record[target_attr] in val_freq):
             val_freq[record[target_attr]] += 1.0
         else:
-            val_freq[record[target_attr]]  = 1.0
+            val_freq[record[target_attr]] = 1.0
 
     # Calculate the entropy of the data for the target attribute
     for freq in val_freq.values():
@@ -31,21 +32,21 @@ def gain(data, attr, target_attr):
     Calculates the information gain (reduction in entropy) that would
     result by splitting the data on the chosen attribute (attr).
     """
-    val_freq       = {}
+    val_freq = {}
     subset_entropy = 0.0
 
     # Calculate the frequency of each of the values in the target attribute
     for record in data:
-        if (val_freq.has_key(record[attr])):
+        if (record[attr] in val_freq):
             val_freq[record[attr]] += 1.0
         else:
-            val_freq[record[attr]]  = 1.0
+            val_freq[record[attr]] = 1.0
 
     # Calculate the sum of the entropy for each subset of records weighted
     # by their probability of occuring in the training set.
     for val in val_freq.keys():
-        val_prob        = val_freq[val] / sum(val_freq.values())
-        data_subset     = [record for record in data if record[attr] == val]
+        val_prob = val_freq[val] / sum(val_freq.values())
+        data_subset = [record for record in data if record[attr] == val]
         subset_entropy += val_prob * entropy(data_subset, target_attr)
 
     # Subtract the entropy of the chosen attribute from the entropy of the
@@ -57,8 +58,8 @@ def create_decision_tree(data, attributes, target_attr, fitness_func):
     """
     Returns a new decision tree based on the examples given.
     """
-    data    = data[:]
-    vals    = [record[target_attr] for record in data]
+    data = data[:]
+    vals = [record[target_attr] for record in data]
     #vals是最终属性所有值的list
     default = majority_value(data, target_attr)
 
@@ -75,7 +76,7 @@ def create_decision_tree(data, attributes, target_attr, fitness_func):
 
         # Create a new decision tree/node with the best attribute and an empty
         # dictionary object--we'll fill that up next.
-        tree = {best:{}}
+        tree = {best: {}}
 
         #通过best属性的各个取值分别创建子树
         for val in get_values(data, best):
@@ -87,7 +88,7 @@ def create_decision_tree(data, attributes, target_attr, fitness_func):
                 fitness_func)
 
             # 将子树添加到树的相应位置
-            # Add the new subtree to the empty dictionary object in our new 
+            # Add the new subtree to the empty dictionary object in our new
             # tree/node we just created.
             tree[best][val] = subtree
 
@@ -105,12 +106,14 @@ This module holds functions that are responsible for creating a new
 decision tree and for using the tree for data classificiation.
 """
 
+
 def majority_value(data, target_attr):
     """
     创建一个包含data中target_attr所有值的列表，返回出现频率最高的值
     """
     data = data[:]
     return most_frequent([record[target_attr] for record in data])
+
 
 def most_frequent(lst):
     """
@@ -127,6 +130,7 @@ def most_frequent(lst):
 
     return most_freq
 
+
 def unique(lst):
     """
     去除列表中的重复项目
@@ -142,12 +146,14 @@ def unique(lst):
     # 返回去除重复值的list
     return unique_lst
 
+
 def get_values(data, attr):
     """
     剪去重复数值,返回列表
     """
     data = data[:]
     return unique([record[attr] for record in data])
+
 
 def choose_attribute(data, attributes, target_attr, fitness):
     """
@@ -165,6 +171,7 @@ def choose_attribute(data, attributes, target_attr, fitness):
 
     return best_attr
 
+
 def get_examples(data, attr, value):
     """
     返回数据集中满足attr值的记录list
@@ -181,6 +188,7 @@ def get_examples(data, attr, value):
 
         return rtn_lst
 
+
 def get_classification(record, tree):
     """
     This function recursively traverses the decision tree and returns a
@@ -188,14 +196,15 @@ def get_classification(record, tree):
     """
     # If the current node is a string, then we've reached a leaf node and
     # we can return it as our answer
-    if type(tree) == type("string"):
+    if isinstance(tree, str):
         return tree
 
     # Traverse the tree further until a leaf node is found.
     else:
-        attr = tree.keys()[0]
+        attr = list(tree.keys())[0]
         t = tree[attr][record[attr]]
         return get_classification(record, t)
+
 
 def classify(tree, data):
     """
@@ -209,4 +218,3 @@ def classify(tree, data):
         classification.append(get_classification(record, tree))
 
     return classification
-
--- run.py
+++ run.py
@@ -1,26 +1,29 @@
 # -*- coding:utf-8 -*-
 
 import sys
-from dtree import *
+import dtree
 
 """
 调用ID3算法的主函数是run_app。
 
-运行(暂时只支持python2.7)
-python run.py train.dat test.dat
+运行(支持python2.7, 3.x)
+python2 run.py train.dat test.dat
+python3 run.py train.dat test.dat
 或者使用ori数据集
-python run.py train-ori.dat test-ori.dat
+python2 run.py train-ori.dat test-ori.dat
+python3 run.py train-ori.dat test-ori.dat
 
 
 数据格式
 
-create_decision_tree(examples, attributes, target_attribute, heuristic_funtion) 
+dtree.create_decision_tree(examples, attributes, target_attribute,
+                           heuristic_funtion)
 接受如下输入:
 
 examples (训练or测试数据集) : list of dicts (python字典)
 attributes : list
 target_attribute: string
-heuristic_funtion:  指向"gain"函数的函数指针
+heuristic_funtion:  指向"dtree.gain"函数的函数指针
 
 数据集文件最后一列为最终决定属性
 """
@@ -32,15 +35,16 @@ def get_training_file():
     """
 
     if len(sys.argv) < 3:
-        print "Please enter the training file: ",
+        print("Please enter the training file: ",)
         training_filename = sys.stdin.readline().strip()
     else:
         training_filename = sys.argv[1]
 
     try:
-        fTrainIn = open(training_filename,"r")
+        fTrainIn = open(training_filename, "r")
     except IOError:
-        print "Error: Could not find the training file specified or unable to open it" %training_filename
+        print("Error: Could not find the training file specified or unable "
+              "to open it" % training_filename)
         sys.exit(0)
 
     return fTrainIn
@@ -52,15 +56,16 @@ def get_test_file():
     """
 
     if len(sys.argv) < 3:
-        print "Please enter the test file: ",
+        print("Please enter the test file: ",)
         test_filename = sys.stdin.readline().strip()
     else:
         test_filename = sys.argv[2]
 
     try:
-        fTestIn = open(test_filename,"r")
+        fTestIn = open(test_filename, "r")
     except IOError:
-        print "Error: Could not find the test file specified or unable to open it" %test_filename
+        print("Error: Could not find the test file specified or unable to "
+              "open it" % test_filename)
         sys.exit(0)
 
     return fTestIn
@@ -76,13 +81,12 @@ def prepare_attributes(attrList):
     attrs = []
     attrsDict = {}
 
-    for i in xrange(0, len(attrList)-1, 2):
+    for i in range(0, len(attrList)-1, 2):
         attrs.append(attrList[i])
         # set the value of attribute name as key to its number
         attrsDict[attrList[i]] = attrList[i+1]
 
     return attrs, attrsDict
-
 
 
 def run_app(fTrainIn, fTestIn):
@@ -95,7 +99,7 @@ def run_app(fTrainIn, fTestIn):
 
     #反转后，利用list.pop()去除最后一行，再反转回到原次序
     linesInTest.reverse()
-    linesInTest.pop() #pop()弹出并返回最后一行
+    linesInTest.pop()    # pop()弹出并返回最后一行
     linesInTest.reverse()
 
     attrList, attrDict = prepare_attributes(attributes)
@@ -104,56 +108,60 @@ def run_app(fTrainIn, fTestIn):
     # prepare testdata
     testData = []
     for line in linesInTest:
-        testData.append(dict(zip(attrList,[datum.strip() for datum in line.split("\t")])))
-
-
+        testData.append(dict(list(zip(attrList,
+                                      [datum.strip()
+                                       for datum in line.split("\t")]))))
 
     linesInTrain = [lineTrain.strip() for lineTrain in fTrainIn.readlines()]
-    attributesTrain = linesInTrain[0].replace("\t"," ").split(" ")
+    attributesTrain = linesInTrain[0].replace("\t", " ").split(" ")
 
     #once we have the attributes remove it from lines
     linesInTrain.reverse()
-    linesInTrain.pop() # pops from end of list, hence the two reverses
+    linesInTrain.pop()   # pops from end of list, hence the two reverses
     linesInTrain.reverse()
 
     attrListTrain, attrDictTrain = prepare_attributes(attributesTrain)
-    targetAttributeTrain = attrListTrain[-1]
+    targetAttrTrain = attrListTrain[-1]
 
     # prepare data
     trainData = []
     for lineTrain in linesInTrain:
-        trainData.append(dict(zip(attrListTrain,[datum.strip() for datum in lineTrain.split("\t")])))
+        trainData.append(dict(list(zip(attrListTrain,
+                                       [datum.strip()
+                                        for datum in lineTrain.split("\t")]))))
 
+    trainingTree = dtree.create_decision_tree(trainData, attrListTrain,
+                                              targetAttrTrain, dtree.gain)
+    trainingClassify = dtree.classify(trainingTree, trainData)
 
+    testTree = dtree.create_decision_tree(testData, attrList, targetAttribute,
+                                          dtree.gain)
+    testClassify = dtree.classify(testTree, testData)
 
-    trainingTree = create_decision_tree(trainData, attrListTrain, targetAttributeTrain, gain)
-    trainingClassification = classify(trainingTree, trainData)
-
-    testTree = create_decision_tree(testData, attrList, targetAttribute, gain)
-    testClassification = classify(testTree, testData)
-
-    # also returning the example classification in both the files
-    givenTestClassification = []
+    # also returning the example Classify in both the files
+    givenTestClassify = []
     for row in testData:
-        givenTestClassification.append(row[targetAttribute])
+        givenTestClassify.append(row[targetAttribute])
 
-    givenTrainClassification = []
+    givenTrainClassify = []
     for row in trainData:
-        givenTrainClassification.append(row[targetAttributeTrain])
+        givenTrainClassify.append(row[targetAttrTrain])
 
-    return trainingTree, trainingClassification, testClassification, givenTrainClassification, givenTestClassification
+    return (trainingTree, trainingClassify, testClassify, givenTrainClassify,
+            givenTestClassify)
 
 
-def accuracy(algoclassification, targetClassification):
+def accuracy(algoClassify, targetClassify):
     matching_count = 0.0
 
-    for alg, target in zip(algoclassification,targetClassification):
+    for alg, target in zip(algoClassify, targetClassify):
         if alg == target:
             matching_count += 1.0
 
-    # print len(algoclassification)
-    # print len(targetClassification)
-    return (matching_count / len(targetClassification)) * 100
+    # print len(algoClassify)
+    # print len(targetClassify)
+    return (matching_count / len(targetClassify)) * 100
+
 
 def print_tree(tree, str):
     """
@@ -161,27 +169,24 @@ def print_tree(tree, str):
     print the tree
     """
 
-    if type(tree)== dict:
-        #print "%s%s = " % (str,tree.keys()[0]),
-        for item in tree.values()[0].keys():
-            print "%s%s = %s" % (str, tree.keys()[0],item),
-            print " : "
-            print "|",
-            print_tree(tree.values()[0][item],str + "  ")
+    if isinstance(tree, dict):
+        # print("%s%s = " % (str, list(tree.keys())[0]))
+        for item in list(list(tree.values())[0].keys()):
+            print("  |  %s%s = %s" % (str, list(tree.keys())[0], item))
+            print_tree(list(tree.values())[0][item], str + "  ")
     else:
-        print "%s : %s" % (str, tree)
+        print("  |  %s : %s" % (str, tree))
 
 if __name__ == "__main__":
 
     fTrainIn = get_training_file()
     fTestIn = get_test_file()
-    trainingTree, trainingClassification, testClassification, givenTrainClassification, givenTestClassification = run_app(fTrainIn,fTestIn)
-    print_tree(trainingTree,"")
-    print " Accuracy of training set (%s instances) : " % len(givenTrainClassification),
-    print accuracy(trainingClassification, givenTrainClassification)
-    print " Accuracy of test set (%s instances) : " % len(givenTestClassification),
-    print accuracy(testClassification, givenTestClassification)
-
-
-
-
+    (trainingTree, trainingClassify, testClassify, givenTrainClassify,
+     givenTestClassify) = run_app(fTrainIn, fTestIn)
+    print_tree(trainingTree, "")
+    print(" Accuracy of training set (%s instances) :  %s"
+          % (len(givenTrainClassify),
+             accuracy(trainingClassify, givenTrainClassify)))
+    print(" Accuracy of test set (%s instances) :  %s"
+          % (len(givenTestClassify),
+             accuracy(testClassify, givenTestClassify)))
